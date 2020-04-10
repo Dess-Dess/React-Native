1ee3b2ff9b8844ac5a16a0183619278af8724bdf
Ruslan <adventureriv@gmail.com>
2020-04-10 16:52:54 +0300

    Остановился на 13 уроке

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import {Navbar} from './src/Navbar'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Navbar title="My Todo App"></Navbar>
+      <Navbar title="Todo App"></Navbar>
     </View>
   );
 }
